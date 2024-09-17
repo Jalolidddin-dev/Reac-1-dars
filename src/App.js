c13a7a40ce264958c46176845eb2d43093a3b9ba
Dew0277 <jaloliddina98@gmail.com>
2024-09-17 06:38:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FaGithub } from "react-icons/fa";
+
+import ImagePerson from "./images/Ellipse 1.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="group">
+        <h1>Hello world</h1>
+        <FaGithub className="git_icon" />
+        <h2>Salom dunyo</h2>
+
+        <img src={ImagePerson} className="image" alt="images-person" />
+      </div>
     </div>
   );
 }
